12d760499b0cb465a2f8f526c840cd23f0e6cc47
duwart <danielduarte@alu.ufc.br>
2023-11-08 19:02:31 -0300

    Modifying main.py and index.html

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Use the command `uvicorn main:app --reload` to run
 
 Atividade: Crie um servidor usando a sua linguagem script e framework de preferência. O servidor deve responder com uma pagina html com uma caixa de texto e um botão. Ao clicar no botão, o conteúdo da caixa de texto é enviado para o servidor e um outro número é retornado pelo servidor. Esse numero deve ser igual ao número enviado somado com 2.
 
-Referencies:
+## Sobre o trabalho
+
+Para a implementação do programa, foi utilizada a linguagem python juntamente com o framework fastApi.
+
+## Referencies:
 
 - https://fastapi.tiangolo.com
 - https://fastapi.tiangolo.com/tutorial/header-params/
--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from typing import Dict
 
+# Aqui é criada uma instância de FastApi
 app = FastAPI()
-# In templates will be the html files
+
 templates = Jinja2Templates(directory="templates")
 
+# A função abaixo é responsável por tratar as requisições que vão para a rota '/'
 @app.get("/", response_class=HTMLResponse)
-async def name(request: Request):
+async def root(request: Request):
     context = {"request": request}
     return templates.TemplateResponse("index.html", context)
 
-@app.post("/")
-async def calculate(data: dict):
+@app.post(
+        "/result/", 
+        status_code=status.HTTP_201_CREATED, 
+        response_model=Dict[str, float],
+        summary="Operação de soma",
+        description="Soma o número definido pelo usuário com o valor 2",
+    )
+async def calculate(data: Dict[str, float]):
     result = data.get("number")
     
     if result is not None:
